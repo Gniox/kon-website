a90785392cf8c81412f0a8c1820f4e2f16dda69f
Gniox <gniox15@gmail.com>
2020-09-13 08:17:08 -0500

    adding functionality to shop.

--- scripts.js
+++ scripts.js
@@ -11,6 +11,9 @@
 //   document.getElementById(id).background = bg;
 // }
 
+let total = 0;
+let num = 1;
+
 function opacityChange(id, opNum) {
   console.log(id);
   document.getElementById(id).style.opacity = opNum;
@@ -29,8 +32,65 @@ function transition(id, page) {
   }, 1000);
 }
 
-// function toggleAnimation(id) {
-//   var element = document.getElementById(id);
-//   element.className = "shake";
-//   console.log("hello");
-// }
+function changeTotal() {
+  document.getElementById("total").innerHTML = total + " headpats";
+}
+
+function remove(id) {
+  let object = document.getElementById(id);
+  total -= parseInt(object.getElementsByClassName("shop-item-price")[0].textContent.match(/(\d+)/));
+  changeTotal();
+  document.getElementById(id).remove();
+}
+
+function quantityChange(quantity) {
+  total += parseInt(
+    item.getElementsByClassName("shop-item-price")[0].textContent.match(/(\d+)/) * quantity
+  );
+
+}
+
+function addToCart(id) {
+  let uniqueId = "cart-item-" + num;
+  let item = document.getElementById(id);
+  let object = document.createElement("div");
+  let pic = document.createElement("img");
+  let name = document.createElement("span");
+  let price = document.createElement("span");
+  let quantity = document.createElement("INPUT");
+  let removeButton = document.createElement("button");
+
+  object.id = uniqueId;
+  num++;
+  console.log(
+    item.getElementsByClassName("shop-item-image")[0].getAttribute("src")
+  );
+  pic.src = item
+    .getElementsByClassName("shop-item-image")[0]
+    .getAttribute("src");
+  pic.classList.add("cart-image");
+  name.textContent = item.getElementsByClassName(
+    "shop-item-title"
+  )[0].textContent;
+  name.classList.add("shop-item-title");
+  price.textContent = item.getElementsByClassName(
+    "shop-item-price"
+  )[0].textContent;
+  total += parseInt(
+    item.getElementsByClassName("shop-item-price")[0].textContent.match(/(\d+)/)
+  );
+  quantity.setAttribute("value", 1);
+  quantity.onchange = function() {quantityChange(parseInt(quantity.getAttribute("value")))};
+  removeButton.innerHTML = "REMOVE";
+  removeButton.onclick = function() {remove(uniqueId)};
+  price.classList.add("shop-item-price");
+  object.appendChild(pic);
+  object.appendChild(name);
+  object.appendChild(price);
+  object.appendChild(quantity);
+  object.appendChild(removeButton);
+  object.classList.add("shop-row");
+  document.getElementById("shop-container").appendChild(object);
+  console.log(total);
+  changeTotal();
+}
